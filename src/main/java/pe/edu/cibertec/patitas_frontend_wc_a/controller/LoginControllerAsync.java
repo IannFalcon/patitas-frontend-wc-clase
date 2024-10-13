0130fecbb31e54652b9c8b86f6c10ace79581d9c
Iann <falconcolos@gmail.com>
2024-10-13 13:07:25 -0500

    Se implemento el servicio para cerrar sesión de forma asincrona

--- src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/controller/LoginControllerAsync.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/controller/LoginControllerAsync.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import pe.edu.cibertec.patitas_frontend_wc_a.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_frontend_wc_a.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_frontend_wc_a.dto.LogoutRequestDTO;
+import pe.edu.cibertec.patitas_frontend_wc_a.dto.LogoutResponseDTO;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -51,5 +53,42 @@ public class LoginControllerAsync {
         }
 
     }
+
+    @PostMapping("/cerrar-sesion-async")
+    public Mono<LogoutResponseDTO> cerrarSesion(@RequestBody LogoutRequestDTO logoutRequestDTO) {
+
+        // Validar campos de entrada
+        if (logoutRequestDTO.tipoDocumento() == null || logoutRequestDTO.tipoDocumento().trim().isEmpty() ||
+            logoutRequestDTO.numeroDocumento() == null || logoutRequestDTO.numeroDocumento().trim().isEmpty()) {
+
+            return  Mono.just(new LogoutResponseDTO("01", "Error: Debe completar correctamente sus credenciales"));
+
+        }
+
+        try {
+
+            // Realizamos la solicitud al servicio
+            return webClientAutenticacion.post()
+                    .uri("http://localhost:8081/autenticacion/logout")
+                    .body(Mono.just(logoutRequestDTO), LogoutRequestDTO.class)
+                    .retrieve()
+                    .bodyToMono(LogoutResponseDTO.class)
+                    .flatMap(response -> {
+                        if(response.codigo().equals("00")) {
+                            return Mono.just(new LogoutResponseDTO("00", ""));
+                        } else {
+                            return Mono.just(new LogoutResponseDTO("02", "Error: No se puedo cerrar sesión"));
+                        }
+                    });
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            return Mono.just(new LogoutResponseDTO("99", "Error: Ocurrió un problema en al cerrar sesión."));
+
+        }
+
+    }
+
 
 }
